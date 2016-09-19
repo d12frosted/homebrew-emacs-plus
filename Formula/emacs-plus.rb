2a44c50438b998fff0a52bdb59c91e9162bf1413
d12frosted <boris@d12frosted.io>
2016-09-19 15:02:56 +0300

    sync formulae

--- Formula/emacs-plus.rb
+++ Formula/emacs-plus.rb
@@ -1,24 +1,14 @@
 class EmacsPlus < Formula
   desc "GNU Emacs text editor"
   homepage "https://www.gnu.org/software/emacs/"
-  url "http://ftpmirror.gnu.org/emacs/emacs-24.5.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.xz"
-  sha256 "dd47d71dd2a526cf6b47cb49af793ec2e26af69a0951cc40e43ae290eacfc34e"
+  url "https://ftpmirror.gnu.org/emacs/emacs-25.1.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/emacs/emacs-25.1.tar.xz"
+  sha256 "19f2798ee3bc26c95dca3303e7ab141e7ad65d6ea2b6945eeba4dbea7df48f33"
 
   bottle do
-    revision 3
-    sha256 "7efa8afcc662120bee21e692e6721a956cb3088f91a6f73fd64252f9679bfc21" => :el_capitan
-    sha256 "77bbc9a112c6107fbcbcbf0112012831235082c6425070a5b092bf25ef84d565" => :yosemite
-    sha256 "f1539cddd0392906fbfdf99e8f20fc07620daaeceac86fa2f98b701cc4f25a3a" => :mavericks
-  end
-
-  devel do
-    url "http://alpha.gnu.org/gnu/emacs/pretest/emacs-25.1-rc2.tar.xz"
-    version "25.1-rc2"
-    sha256 "5bd45f03bdff90f9d7add7224917fc828ed89716e952b3db8eb98242b7dfcec1"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
+    sha256 "6022295cbbad123db684cef19029d6100e711e29c160ac9ba1bb7a38304655da" => :sierra
+    sha256 "013398eb1c8030b31423484bc0c316245cbab523c70452f200814950c98b1f44" => :el_capitan
+    sha256 "fa3f4f8f6050072e2032c7dc04d3289ec82847bb2ea507c1444bbc385f375eda" => :yosemite
   end
 
   head do
@@ -31,6 +21,7 @@ class EmacsPlus < Formula
   option "with-cocoa", "Build a Cocoa version of emacs"
   option "with-ctags", "Don't remove the ctags executable that emacs provides"
   option "without-libxml2", "Don't build with libxml2 support"
+  option "with-modules", "Compile with dynamic modules support"
   option "with-spacemacs-icon", "Using the spacemacs Emacs icon by Nasser Alshammari"
 
   deprecated_option "cocoa" => "with-cocoa"
@@ -43,9 +34,6 @@ class EmacsPlus < Formula
   depends_on "librsvg" => :recommended
   depends_on "imagemagick" => :optional
   depends_on "mailutils" => :optional
-  # Remove this option and the --with-file-notification=gfile line below once
-  # Emacs 25 is stable (#4048)
-  depends_on "glib" => :optional
 
   def install
     args = %W[
@@ -56,8 +44,6 @@ class EmacsPlus < Formula
       --prefix=#{prefix}
       --without-x
     ]
-
-    args << "--with-file-notification=gfile" if build.stable? && build.with?("glib")
 
     if build.with? "libxml2"
       args << "--with-xml2"
@@ -77,9 +63,10 @@ class EmacsPlus < Formula
       args << "--without-gnutls"
     end
 
-    args << "--with-rsvg" if build.with? "librsvg"
     args << "--with-imagemagick" if build.with? "imagemagick"
-    args << "--without-popmail" if build.with? "mailutils"
+    args << "--with-modules" if build.with? "modules"
+    args << "--with-rsvg" if build.with? "librsvg"
+    args << "--without-pop" if build.with? "mailutils"
 
     system "./autogen.sh" if build.head? || build.devel?
 
@@ -94,14 +81,6 @@ class EmacsPlus < Formula
     system "make", "install"
 
     if build.with? "cocoa"
-      # Remove when 25.1 is released
-      if build.stable?
-        chmod 0644, %w[nextstep/Emacs.app/Contents/PkgInfo
-                       nextstep/Emacs.app/Contents/Resources/Credits.html
-                       nextstep/Emacs.app/Contents/Resources/document.icns
-                       nextstep/Emacs.app/Contents/Resources/Emacs.icns]
-      end
-
       # icons
       if build.with? "spacemacs-icon"
         icon_file = "nextstep/Emacs.app/Contents/Resources/Emacs.icns"
@@ -136,7 +115,7 @@ class EmacsPlus < Formula
     end
   end
 
-  plist_options :manual => "emacs"
+  plist_options manual: "emacs"
 
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
