3aaed8290bec3f1c678243274fd4ed0f37ae83e7
Julien Wintz <julien.wintz@inria.fr>
2017-03-31 10:53:24 +0200

    Removing dark vibrant appearance that messes up themes on some setups.

--- Formula/emacs-plus.rb
+++ Formula/emacs-plus.rb
@@ -78,8 +78,8 @@ class EmacsPlus < Formula
 
   if build.with? "natural-title-bar"
     patch do
-      url "https://gist.githubusercontent.com/jwintz/853f0075cf46770f5ab4f1dbf380ab11/raw/bc30bd2e9a7bf6873f3a3e301d0085bcbefb99b0/emacs_dark_title_bar.patch"
-      sha256 "742f7275f3ada695e32735fa02edf91a2ae7b1fa87b7e5f5c6478dd591efa162"
+      url "https://gist.githubusercontent.com/jwintz/853f0075cf46770f5ab4f1dbf380ab11/raw/2d7703f380f421baa26e5a5fd8b9044aa3a5ea3e/emacs_dark_title_bar.patch"
+      sha256 "1eaac4a22d9f0dd86f09d7038a9cc413a1f06ceb32efefa4ee4883bd25b19028"
     end
   end
 
