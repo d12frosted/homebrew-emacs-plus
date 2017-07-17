66ecffa0779cc457139b0597287bcb997085ca0d
d12frosted <boris@d12frosted.io>
2017-07-17 09:22:46 +0300

    update bottle for sierra

--- Formula/emacs-plus.rb
+++ Formula/emacs-plus.rb
@@ -7,8 +7,9 @@ class EmacsPlus < Formula
 
   bottle do
     root_url "https://dl.dropboxusercontent.com/u/14438695/emacs-plus/bottles"
-    sha256 "aca2af5484f05d06c67273f50fe8851a27f48ef0e265fba1fe00456bdab504de" => :sierra
-    sha256 "0cad5513de68ec93b4f522fd6a065ee163bc880a5e8091794fb8b3da3dc635e5" => :el_capitan
+    rebuild 1
+    sha256 "f6fa36d3afb47d8251fdade4d967deca9fac470ad029b6db04650643f2623d1f" => :sierra
+    # sha256 "0cad5513de68ec93b4f522fd6a065ee163bc880a5e8091794fb8b3da3dc635e5" => :el_capitan
   end
 
   head do
