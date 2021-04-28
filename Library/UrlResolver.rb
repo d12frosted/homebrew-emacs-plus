ae19fa3b0edc622ce0bfeca05130c4db58b4dab8
Boris Buliga <boris@d12frosted.io>
2021-04-28 21:43:53 +0300

    handle missing formula during initialisation
    Whenever a tap is tapped, its formulas are being loaded. But since it
    happens before formula becomes available, `Formula[name]` leads to
    infinite recursion. Since we care about the value of `@formula_dir`
    during installation, and not during initialisation, it's safe to
    ignore any errors there.
    
    Fixes #348

--- Library/UrlResolver.rb
+++ Library/UrlResolver.rb
@@ -2,10 +2,15 @@ class UrlResolver
   def initialize(version, mode)
     @version = version
     @formula_name = "emacs-plus@#{version}"
-    @formula_dir =
-      mode == "local" ?
-        Dir.pwd :
-        (Formula[@formula_name].path.to_s.delete_suffix "/Formula/#@formula_name.rb")
+    @formula_dir = mode == "local" ? Dir.pwd : (UrlResolver.formula_root @formula_name)
+  end
+
+  def self.formula_root name
+    begin
+      Formula[@formula_name].path.to_s.delete_suffix "/Formula/#@formula_name.rb"
+    rescue
+      Dir.pwd
+    end
   end
 
   def patch_url name
