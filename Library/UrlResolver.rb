a25e1eea70ca7198ad28fb8c1338d175527bfb3e
Boris Buliga <boris@d12frosted.io>
2021-04-28 18:55:26 +0300

    revert latest changes to resource loading

--- Library/UrlResolver.rb
+++ Library/UrlResolver.rb
@@ -1,18 +1,22 @@
 class UrlResolver
-  def initialize(version, mode)
-    @version = version
-    @formula_name = "emacs-plus@#{version}"
-    @formula_dir =
-      mode == "local" ?
-        Dir.pwd :
-        (Formula[@formula_name].path.to_s.delete_suffix "/Formula/#@formula_name.rb")
+  def self.repo
+    (ENV["HOMEBREW_GITHUB_ACTOR"] or "d12frosted") + "/" + "homebrew-emacs-plus"
   end
 
-  def patch_url name
-    "#@formula_dir/patches/emacs-#@version/#{name}.patch"
+  def self.branch
+    ref = ENV["HOMEBREW_GITHUB_REF"]
+    if ref
+      ref.sub("refs/heads/", "")
+    else
+      "master"
+    end
   end
 
-  def icon_url name
-    "#@formula_dir/icons/#{name}.icns"
+  def self.patch_url(name)
+    "https://raw.githubusercontent.com/#{repo}/#{branch}/patches/#{name}.patch"
+  end
+
+  def self.icon_url(name)
+    "https://raw.githubusercontent.com/#{repo}/#{branch}/icons/#{name}.icns"
   end
 end
