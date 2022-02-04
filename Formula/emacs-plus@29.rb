26b7829066ebfdbc9c5c41eadafa0ecf83d8a933
Boris Buliga <boris@d12frosted.io>
2022-02-04 12:26:40 +0200

    (emacs-29) deprecate no-titlebar option
    See #433

--- Formula/emacs-plus@29.rb
+++ Formula/emacs-plus@29.rb
@@ -14,7 +14,6 @@ class EmacsPlusAT29 < EmacsBase
   # Opt-in
   option "with-ctags", "Don't remove the ctags executable that Emacs provides"
   option "with-x11", "Experimental: build with x11 support"
-  option "with-no-titlebar", "Experimental: build without titlebar"
   option "with-debug", "Build with debug symbols and debugger friendly optimizations"
   option "with-xwidgets", "Experimental: build with xwidgets support"
   option "with-no-frame-refocus", "Disables frame re-focus (ie. closing one frame does not refocus another one)"
@@ -76,10 +75,22 @@ class EmacsPlusAT29 < EmacsBase
   # Patches
   #
 
-  local_patch "no-titlebar", sha: "2fa80efc5cda7e96d88a5d145c9313092a6e53d38825c41967c745f08778c41b" if build.with? "no-titlebar"
   local_patch "no-frame-refocus-cocoa", sha: "fb5777dc890aa07349f143ae65c2bcf43edad6febfd564b01a2235c5a15fcabd" if build.with? "no-frame-refocus"
   local_patch "fix-window-role", sha: "1f8423ea7e6e66c9ac6dd8e37b119972daa1264de00172a24a79a710efcb8130"
   local_patch "system-appearance", sha: "d6ee159839b38b6af539d7b9bdff231263e451c1fd42eec0d125318c9db8cd92"
+
+  #
+  # no-titlebar deprecation
+  #
+
+  option "with-no-titlebar", "Deprecated, will be removed on 01.03.2022"
+  if build.with? "no-titlebar"
+    odie <<~EOS
+      --with-no-titlebar is not available anymore, instead add the following in early-init.el
+
+        (add-to-list 'default-frame-alist '(undecorated . t))
+    EOS
+  end
 
   #
   # Install
