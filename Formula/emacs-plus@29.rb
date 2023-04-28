e58637205a6d3a259503d3e8d8941bd660fa7b98
Daniel Fleischer <danflscr@gmail.com>
2023-04-28 13:24:33 +0300

    Missing tap argument
    See
    https://github.com/Homebrew/brew/commit/e191b827cce991f52be36b0a8fc3daed3ef66143.

--- Formula/emacs-plus@29.rb
+++ Formula/emacs-plus@29.rb
@@ -94,7 +94,7 @@ class EmacsPlusAT29 < EmacsBase
   # Initialize
   #
 
-  def initialize(name, path, spec, alias_path: nil, force_bottle: false)
+  def initialize(name, path, spec, alias_path: nil, force_bottle: false, tap: nil)
     super
     expand_path
   end
