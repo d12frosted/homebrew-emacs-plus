629c8fd6c7159ea487209bc8bcc95dfea70547f7
Boris Buliga <boris@d12frosted.io>
2023-04-29 12:51:09 +0300

    fix: future proof initialize wrapper

--- Formula/emacs-plus@29.rb
+++ Formula/emacs-plus@29.rb
@@ -94,9 +94,13 @@ class EmacsPlusAT29 < EmacsBase
   # Initialize
   #
 
-  def initialize(name, path, spec, alias_path: nil, force_bottle: false, tap: nil)
-    super
+  # Save the existing method.
+  alias :initialize_old :initialize
+
+  def initialize(*args, &block)
+    a = initialize_old(*args, &block)
     expand_path
+    a
   end
 
   #
