b926ff102067d3864ff4cb8060962ec4d46510ef
jidicula <git@johanan.dev>
2023-05-08 13:45:46 -0400

    Revert "fix: Revert #478"
    This reverts commit 9e9c777d2e28de6ca3201c8c3134cddedaa9dd55.

--- Formula/emacs-plus@29.rb
+++ Formula/emacs-plus@29.rb
@@ -200,6 +200,7 @@ class EmacsPlusAT29 < EmacsBase
 
       # (prefix/"share/emacs/#{version}").install "lisp"
       prefix.install "nextstep/Emacs.app"
+      (prefix/"Emacs.app/Contents").install "native-lisp" if build.with? "native-comp"
 
       # inject PATH to Info.plist
       inject_path
