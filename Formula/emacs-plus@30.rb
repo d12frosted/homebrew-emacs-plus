51654970f5dd010307149ec243418483b24221d8
Aaron Jensen <aaronjensen@gmail.com>
2023-09-12 00:21:28 -0400

    Correct typo

--- Formula/emacs-plus@30.rb
+++ Formula/emacs-plus@30.rb
@@ -86,7 +86,7 @@ class EmacsPlusAT30 < EmacsBase
   # Patches
   #
 
-  opoo "The option --with-no-frame-refocus is not required anymore in emacs-plud@30." if build.with? "no-frame-refocus"
+  opoo "The option --with-no-frame-refocus is not required anymore in emacs-plus@30." if build.with? "no-frame-refocus"
   local_patch "fix-window-role", sha: "1f8423ea7e6e66c9ac6dd8e37b119972daa1264de00172a24a79a710efcb8130"
   local_patch "system-appearance", sha: "d6ee159839b38b6af539d7b9bdff231263e451c1fd42eec0d125318c9db8cd92"
   local_patch "poll", sha: "052eacac5b7bd86b466f9a3d18bff9357f2b97517f463a09e4c51255bdb14648" if build.with? "poll"
