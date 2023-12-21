d58ea8fc339016188c081c75e479ca64fa4a342c
Dan King <daniel.zidan.king@gmail.com>
2023-12-21 17:23:40 -0500

    Fix linking how-to in formula caveats (#642)
    * Fix link how-to in formula caveats
    
    * Also update emacs 30
    
    * Update emacs-plus@26.rb
    
    * Update emacs-plus@27.rb
    
    * Update emacs-plus@28.rb

--- Formula/emacs-plus@30.rb
+++ Formula/emacs-plus@30.rb
@@ -270,7 +270,7 @@ class EmacsPlusAT30 < EmacsBase
         #{prefix}
 
       To link the application to default Homebrew App location:
-        ln -s #{prefix}/Emacs.app /Applications
+        osascript -e 'tell application "Finder" to make alias file to posix file "#{prefix}/Emacs.app" at POSIX file "/Applications"'
 
       Your PATH value was injected into Emacs.app/Contents/Info.plist
 
