e2869844e8f5e7995e25071cbcbe69f26f707109
Aaron Jensen <aaronjensen@gmail.com>
2023-12-27 13:43:29 -0500

    Emacs 30 system-appearance patch is corrected (#644)

--- Formula/emacs-plus@30.rb
+++ Formula/emacs-plus@30.rb
@@ -88,7 +88,7 @@ class EmacsPlusAT30 < EmacsBase
 
   opoo "The option --with-no-frame-refocus is not required anymore in emacs-plus@30." if build.with? "no-frame-refocus"
   local_patch "fix-window-role", sha: "1f8423ea7e6e66c9ac6dd8e37b119972daa1264de00172a24a79a710efcb8130"
-  local_patch "system-appearance", sha: "d6ee159839b38b6af539d7b9bdff231263e451c1fd42eec0d125318c9db8cd92"
+  local_patch "system-appearance", sha: "9eb3ce80640025bff96ebaeb5893430116368d6349f4eb0cb4ef8b3d58477db6"
   local_patch "poll", sha: "052eacac5b7bd86b466f9a3d18bff9357f2b97517f463a09e4c51255bdb14648" if build.with? "poll"
   local_patch "round-undecorated-frame", sha: "7451f80f559840e54e6a052e55d1100778abc55f98f1d0c038a24e25773f2874"
 
