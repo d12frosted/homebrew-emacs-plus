0e0cfdc2caa9cd5e78091cb203706d3d398ceba2
Ben Chen <517926804@qq.com>
2024-01-26 11:34:53 +0800

    feat: add alpha-background support to emacs 29 and 30

--- Formula/emacs-plus@29.rb
+++ Formula/emacs-plus@29.rb
@@ -93,6 +93,7 @@ class EmacsPlusAT29 < EmacsBase
   local_patch "system-appearance", sha: "d6ee159839b38b6af539d7b9bdff231263e451c1fd42eec0d125318c9db8cd92"
   local_patch "poll", sha: "052eacac5b7bd86b466f9a3d18bff9357f2b97517f463a09e4c51255bdb14648" if build.with? "poll"
   local_patch "round-undecorated-frame", sha: "7451f80f559840e54e6a052e55d1100778abc55f98f1d0c038a24e25773f2874"
+  local_patch "alpha-background", sha: "922d9c5cd7deebd16773d354150faa8a5e69d998651cb2e956d9ed600232b4bc" 
 
   #
   # Initialize
