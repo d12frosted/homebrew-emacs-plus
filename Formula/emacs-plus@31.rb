5c03f0aefe58265a115613e63c7c8aef0696eef2
Boris Buliga <boris@d12frosted.io>
2024-09-17 11:22:13 +0300

    add m4 as dependency for Emacs 31

--- Formula/emacs-plus@31.rb
+++ Formula/emacs-plus@31.rb
@@ -38,6 +38,7 @@ class EmacsPlusAT31 < EmacsBase
   depends_on "pkg-config" => :build
   depends_on "texinfo" => :build
   depends_on "xz" => :build
+  depends_on "m4" => :build 
   depends_on "gnutls"
   depends_on "librsvg"
   depends_on "little-cms2"
