e47b2b7f20c6ec1770010896832054cb649bac15
Aaron Jensen <aaronjensen@gmail.com>
2024-10-30 23:35:32 -0700

    AppleScript usage of posix is made consistent (#743)

--- Formula/emacs-plus@26.rb
+++ Formula/emacs-plus@26.rb
@@ -116,7 +116,7 @@ class EmacsPlusAT26 < EmacsBase
         #{prefix}
 
       To link the application to default Homebrew App location:
-        osascript -e 'tell application "Finder" to make alias file to posix file "#{prefix}/Emacs.app" at POSIX file "/Applications" with properties {name:"Emacs.app"}'
+        osascript -e 'tell application "Finder" to make alias file to posix file "#{prefix}/Emacs.app" at posix file "/Applications" with properties {name:"Emacs.app"}'
     EOS
   end
 
