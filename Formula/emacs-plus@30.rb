19deca441746f0b4cd41a3976c2130589642517c
xenodium <8107219+xenodium@users.noreply.github.com>
2025-02-09 19:08:06 +0000

    Expose macOS native sharing dialog via ns-share
    ns-share can be invoked on:
    
    - Region text
    - Current buffer file
    - Dired marked files
    - Dired region files

--- Formula/emacs-plus@30.rb
+++ Formula/emacs-plus@30.rb
@@ -107,6 +107,7 @@ class EmacsPlusAT30 < EmacsBase
   local_patch "fix-window-role", sha: "1f8423ea7e6e66c9ac6dd8e37b119972daa1264de00172a24a79a710efcb8130"
   local_patch "system-appearance", sha: "9eb3ce80640025bff96ebaeb5893430116368d6349f4eb0cb4ef8b3d58477db6"
   local_patch "round-undecorated-frame", sha: "7451f80f559840e54e6a052e55d1100778abc55f98f1d0c038a24e25773f2874"
+  local_patch "native-sharing", sha: "75f3e7ca136c3901582d8ee3fb5a9833ba010280e13097048c9dd3405f18a178"
 
   #
   # Install
