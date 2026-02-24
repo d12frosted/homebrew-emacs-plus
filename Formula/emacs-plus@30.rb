3d2bd26bd518517dc76c32550c41e90b7ae95ec3
Boris Buliga <boris@d12frosted.io>
2026-02-24 11:16:58 +0200

    fix(emacs-plus@30): add explicit version to prevent nil when using custom revision
    When a custom revision is set in build.yml, the stable URL is replaced
    with a git URL that contains no version information, causing Homebrew to
    fail with "invalid attribute for formula: version (nil)".
    
    Adding an explicit version declaration ensures the version is always
    defined regardless of URL overrides.
    
    Closes #919

--- Formula/emacs-plus@30.rb
+++ Formula/emacs-plus@30.rb
@@ -2,6 +2,7 @@ require_relative "../Library/EmacsBase"
 
 class EmacsPlusAT30 < EmacsBase
   init 30
+  version "30.2"
   url "https://ftpmirror.gnu.org/emacs/emacs-30.2.tar.xz"
   mirror "https://ftp.gnu.org/gnu/emacs/emacs-30.2.tar.xz"
   sha256 "b3f36f18a6dd2715713370166257de2fae01f9d38cfe878ced9b1e6ded5befd9"
