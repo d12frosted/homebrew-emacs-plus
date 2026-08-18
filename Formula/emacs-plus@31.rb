942b5334a5a26be2b79dbe4e4ed7c3a831a63665
Boris Buliga <boris@d12frosted.io>
2026-08-18 08:30:55 +0300

    chore(emacs-plus@31): update to 31.1
    The emacs-31 branch declares 31.1 since the 31.1 release candidate. The formula builds from that branch, so only the version label changes.

--- Formula/emacs-plus@31.rb
+++ Formula/emacs-plus@31.rb
@@ -1,7 +1,7 @@
 require_relative "../Library/EmacsBase"
 
 class EmacsPlusAT31 < EmacsBase
-  init "31.0.91", branch: "emacs-31"
+  init "31.1", branch: "emacs-31"
 
   desc "GNU Emacs text editor"
   homepage "https://www.gnu.org/software/emacs/"
